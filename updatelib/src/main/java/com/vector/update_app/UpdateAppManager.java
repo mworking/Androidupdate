3cdc2860fab7cef335769012ba00efc413b97936
himici <jianfeng.ge@1rock.cn>
2017-11-27 15:50:05 +0800

    add

--- updatelib/src/main/java/com/vector/update_app/UpdateAppManager.java
+++ updatelib/src/main/java/com/vector/update_app/UpdateAppManager.java
@@ -67,36 +67,6 @@ public class UpdateAppManager {
     }
 
     /**
-     * 可以直接利用下载功能，
-     *
-     * @param context          上下文
-     * @param updateAppBean    下载信息配置
-     * @param downloadCallback 下载回调
-     */
-    public static void download(final Context context, @NonNull final UpdateAppBean updateAppBean, @Nullable final DownloadService.DownloadCallback downloadCallback) {
-
-        if (updateAppBean == null) {
-            throw new NullPointerException("updateApp 不能为空");
-        }
-
-        DownloadService.bindService(context.getApplicationContext(), new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                ((DownloadService.DownloadBinder) service).start(updateAppBean, downloadCallback);
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-
-            }
-        });
-    }
-
-    public Context getContext() {
-        return mActivity;
-    }
-
-    /**
      * 自定义更新dialog
      */
     public void showDialogFragment() {
